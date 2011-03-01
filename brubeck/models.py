68585925fec8d8a4c747524cc74c010ce1b0eca2
James Dennis <jd@j2labs.net>
2011-03-01 03:30:58 -0500

    First draft at some authentication handling. Initial design inspired by tornado.

---
+++ brubeck/models.py
@@ -0,0 +1,123 @@
+import datetime
+import math
+import logging
+from dateutil.parser import parse
+import re
+import time
+import uuid
+
+
+###
+### DictShield documents
+###
+
+from dictshield.base import BaseField, DictPunch
+from dictshield.document import Document
+from dictshield.fields import (StringField,
+                               BooleanField,
+                               URLField,
+                               EmailField,
+                               LongField)
+# this might get moved
+from dictshield.fields import ObjectIdField
+
+import auth
+from request_handling import curtime
+
+###
+### User Document
+###
+
+class User(Document):
+    """Bare minimum to have the concept of a User.
+    """
+    _private_fields = [
+        'password', 'is_active',
+    ]
+
+    username_regex = re.compile('[^a-zA-Z0-9._]')
+    username_min_length = 2
+
+    username = StringField(max_length=30, required=True)
+    email = EmailField(max_length=100)
+    password = StringField(max_length=128)
+    is_active = BooleanField(default=False)
+    last_login = LongField(default=curtime)
+    date_joined = LongField(default=curtime)
+
+    def __unicode__(self):
+        return u'%s' % (self.username)
+
+    def set_password(self, raw_passwd):
+        """Generates bcrypt hash and salt for storing a user's password. With
+        bcrypt, the salt is kind of redundant, but this format stays friendly
+        to other algorithms.
+        """
+        (algorithm, salt, digest) = auth.gen_hexdigest(raw_passwd)
+        self.password = auth.build_passwd_line(algorithm, salt, digest)
+
+    def check_password(self, raw_password):
+        """Compares raw_password to password stored for user. Updates
+        self.last_login on success.
+        """
+        algorithm, salt, hash = auth.split_passwd_line(self.password)
+        (_, _, user_hash) = auth.gen_hexdigest(raw_password,
+                                               algorithm=algorithm, salt=salt)
+        if hash == user_hash:
+            self.last_login = curtime()
+            return True
+        else:
+            return False
+
+    @classmethod
+    def create_user(cls, username, password, email=str()):
+        """Creates a user document with given username and password
+        and saves it.
+
+        Validation occurs only for email argument. It makes no assumptions
+        about password format.
+        """
+        now = curtime()
+        username = username.lower()
+
+        # validate email argument
+        try:
+            cls.validate_class_partial(dict(email=email))
+        except ValueError:
+            pass
+        else:
+            email = email.strip()
+            email = email.lower()
+
+        user = cls(username=username, email=email, date_joined=now)
+        user.set_password(password)
+        return user
+
+
+###
+### UserProfile
+###
+    
+class UserProfile(Document):
+    """The basic things a user profile tends to carry. Isolated in separate
+    class to keep separate from private data.
+    """
+    owner = ObjectIdField(required=True)
+    username = StringField(max_length=30, required=True)
+    name = StringField(max_length=255)
+    website = URLField(max_length=255)
+    bio = StringField(max_length=100)
+    location_text = StringField(max_length=100)
+    avatar_url = URLField(max_length=255)
+
+    _private_fields = [
+        'owner',
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfile, self).__init__(*args, **kwargs)
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
+
---
+++ demo/demoauth.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+
+"""This is a rough prototype
+"""
+
+import sys
+
+from brubeck.request_handling import Brubeck, WebMessageHandler
+from brubeck.models import User
+from brubeck.auth import web_authenticated, UserHandlingMixin
+
+import logging
+log_config = dict(#filename='brubeck.log',
+                  level=logging.DEBUG)
+
+logging.basicConfig(**log_config)
+
+class DemoHandler(WebMessageHandler, UserHandlingMixin):
+    def get_current_user(self):
+        """Receives authentication credentials from a user and attempts
+        to load the relevant user from a database.
+
+        Values hardcoded for the demo.
+        """
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        # hardcode the username and password for the demo
+        demo_user = User.create_user('jd', 'foo')
+        # instead of loading the user by username, we'll match against
+        # the hardcoded value
+        if demo_user.username != username:
+            logging.debug('Auth fail: username incorrect')
+            return
+        # attach user instance to request handler
+        if demo_user.check_password(password):
+            logging.debug('Authentication passed')
+            return demo_user
+        else:
+            logging.debug('Auth fail: password incorrect')
+    
+    @web_authenticated
+    def post(self):
+        """Function called for HTTP POST. Requires username and password."""
+        logging.debug('DemoHandler.post() calling .get()')
+        self.set_body('You logged in successfully!')
+        self.set_status(200)
+        return self.render()
+
+        
+if __name__ == '__main__':
+    pull_addr = 'ipc://127.0.0.1:9999'
+    pub_addr = 'ipc://127.0.0.1:9998'
+
+    # Make sure mongrel2's config is in sync with this.
+    handler_tuples = ((r'', DemoHandler),)
+
+    app = Brubeck((pull_addr, pub_addr), handler_tuples)
+    app.run()
