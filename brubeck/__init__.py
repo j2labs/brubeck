fc5d102aa01ba38aae887e591f42efc36883c5f2
James Dennis <jd@j2labs.net>
2011-03-01 03:33:55 -0500

    Incrementing version

--- brubeck/__init__.py
+++ brubeck/__init__.py
@@ -2,6 +2,6 @@
 
 """The Brubeck Message Handling system."""
 
-version = "0.1.2"
-version_info = (0, 1, 2)
+version = "0.1.3"
+version_info = (0, 1, 3)
 __all__ = [ 'auth', 'models', 'mongrel2', 'request_handling', ]
