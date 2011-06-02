6a570fd3176dcb60b97d01f05a6fc7d8c5a945a9
James Dennis <jd@j2labs.net>
2011-06-02 01:26:23 -0400

    version bump

--- brubeck/__init__.py
+++ brubeck/__init__.py
@@ -2,6 +2,6 @@
 
 """The Brubeck Message Handling system."""
 
-version = "0.2.6"
-version_info = (0, 2, 6)
+version = "0.2.7"
+version_info = (0, 2, 7)
 __all__ = [ 'auth', 'models', 'mongrel2', 'request_handling', 'templating',]
