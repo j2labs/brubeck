983fae6e3939946da7f74a52596d0a2090b44302
James Dennis <jd@j2labs.net>
2011-07-09 01:41:11 -0400

    Adding my config for building documentation markup

---
+++ docs/upupdowndown.py
@@ -0,0 +1,5 @@
+### Could live in settings
+src_dir = './'
+html_dir = '../../brubeck.io/'
+header_file = '%s%s' % (html_dir + 'media/', 'header.html')
+footer_file = '%s%s' % (html_dir + 'media/', 'footer.html')
