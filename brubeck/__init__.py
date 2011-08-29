fb30556e99c17153d9526821c4c284bc492ccf92
James Dennis <jdennis@gmail.com>
2011-08-29 05:33:22 -0400

    Updated version number

--- brubeck/__init__.py
+++ brubeck/__init__.py
@@ -2,6 +2,6 @@
 
 """The Brubeck Message Handling system."""
 
-version = "0.3.2"
-version_info = (0, 3, 2)
+version = "0.3.3"
+version_info = (0, 3, 3)
 __all__ = [ 'auth', 'models', 'mongrel2', 'request_handling', 'templating', 'timekeeping']
