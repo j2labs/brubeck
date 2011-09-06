2330c3c561ede40666ee42de0ed316bf8c885f5b
James Dennis <jd@j2labs.net>
2011-09-06 14:55:58 -0400

    version bump to match completeness of ewn template argument handling

--- brubeck/__init__.py
+++ brubeck/__init__.py
@@ -2,6 +2,6 @@
 
 """The Brubeck Message Handling system."""
 
-version = "0.3.3"
-version_info = (0, 3, 3)
+version = "0.3.4"
+version_info = (0, 3, 4)
 __all__ = [ 'auth', 'models', 'mongrel2', 'request_handling', 'templating', 'timekeeping']
