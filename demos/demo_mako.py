2a78881bfbb99c5de4741aadfa2361e040bc570b
James Dennis <jd@j2labs.net>
2011-11-12 13:33:20 -0500

    formatting

--- demos/demo_mako.py
+++ demos/demo_mako.py
@@ -6,13 +6,14 @@ from brubeck.templating import MakoRendering, load_mako_env
 
 
 class DemoHandler(MakoRendering):
-  def get(self):
-    name = self.get_argument('name', 'dude')
-    context = {'name': name}
-    return self.render_template('success.html', **context)
-
+    def get(self):
+        name = self.get_argument('name', 'dude')
+        context = {
+            'name': name
+        }
+        return self.render_template('success.html', **context)
 
 app = Brubeck(mongrel2_pair=('ipc://127.0.0.1:9999', 'ipc://127.0.0.1:9998'),
-  handler_tuples=[(r'^/brubeck', DemoHandler)],
-  template_loader=load_mako_env('./templates/mako'))
+              handler_tuples=[(r'^/brubeck', DemoHandler)],
+              template_loader=load_mako_env('./templates/mako'))
 app.run()
