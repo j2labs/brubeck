86b816755ea8ca6187653405b3966ce70f7cf5ce
Ben Beecher <benbeecher@gmail.com>
2011-11-17 16:37:32 -0500

    finished logic for apis. Now to hook it in

--- brubeck/models.py
+++ brubeck/models.py
@@ -141,26 +141,60 @@ class APIableMixin(object):
         #register itself with brubeck's manafest
 
 class AutoAPIBase(JSONMessageHandler):
-    def __init__(self, dicttype, queryset):
+    def __init__(self, dicttype, queryset, *args, **kwargs):
         self.dicttype = dicttype
         self.queries = queryset
+        super(AutoAPIBase, self).__init__(*args, **kwargs)
+
+
+    def add_single(self, item_id=None):
+        """Adds a single item to the database - the item should be represented as a object
+        inside a single item list,  in an object with the "data" key.
+        e.g.
+        { 'data' : [
+                    {
+                     'mysamplekey1':somesamplevalue,
+                     'mysamplekey2':somesamplevalue,
+                    }
+                   ]
+        }
+
+        This keeps the interface constant if you're passing a single item or a list of items.
+        We only want to deal with sequences!
+        """
+        
+        #load data from postbody
+        data = json.loads(self.arguments['data'])
+        #ensure it's good data
+        shield = self.dicttype(**data)
+        shield.validate()
+        #serialze and persist the data
+        shield = self.queries.save_single(shield, item_id)
+        #return the local representation of the shield
+        self.add_to_payload('data', json.dumps([shield.to_json()]))
+
 
 
 class APIListHandler(AutoAPIBase):
     def get(self):
+        """Returns a jsonifyed list of objects in the db.
+        """
+        #TODO:Figure out how we want to pagify this or somehow break it down from a single monster rv
         instances = self.queries.get_list()
-        instances = json.dumps(instances)
+        instances = json.dumps([instance.to_json() for instance in instances])
         self.add_to_payload('data', instances)
         return self.render(status_code=200)
+
     def post(self):
-        data = json.loads(self.arguments['data'])
-        shield = self.dicttype(**data)
+        """Adds a single _new_ item to the database and returns a representation of it
+        """
         try:
-            shield.validate()
+            self.add_single()
         except ShieldException:
             return self.render(status_code=400)
-        self.queries.save_single(shield)
-        return self.render(status_code=200)
+
+        return self.render(status_code=201)
+
     #def put(self):
         #replace the entire collection with a new one. This seems dangerous 
     #def delete(self):
@@ -170,10 +204,37 @@ class APIListHandler(AutoAPIBase):
 
 class APISingleHandler(AutoAPIBase):
     def get(self, item_id):
-        instance = self.queries.get_single(item_id)
-        instance = json.dumps([instance])
-        self.add_to_payload('data', instance)
+        """Pull out the representation of an item in the db
+        """
+        try:
+            instance = self.queries.get_single(item_id)
+        except KeyError:
+            return self.render(status_code=404)
+        self.add_to_payload('data', json.dumps([instance.to_json()]))
         return self.render(status_code=200)
+
+    def put(self, item_id):
+        """Updates an item in the db
+        """
+        try:
+            self.add_single(item_id)
+        except ShieldException:
+            return self.render(status_code=400)
+        except KeyError:
+            return self.render(status_code=404)            
+
+        return self.render(status_code=200)
+
+    def delete(self, item_id):
+        """ Deletes an item in the db
+        """
+        try:
+            self.queries.delete_single(item_id)
+        except KeyError:
+            return self.render(status_code=404)
+        return self.render(status_code=200)
+
+
     #def post(self):
         # not sure what to do with this - generalized post to item syntax makes me think that:
         # we should check for collection at location.
@@ -181,29 +242,22 @@ class APISingleHandler(AutoAPIBase):
 
     #def head(self):
         #return metadata about the resource - the headers should be the same as a get requets, but no content body
-    def put(self, item_id):
-        data = json.loads(self.arguments['data'])
-        shield = self.dicttype(**data)
-        try:
-            shield.validate()
-        except ShieldException:
-            return self.render(status_code=400)
-        self.queries.save_single(item_id, shield)
-        return self.render(status_code=200)
-    def delete(self, item_id):
-        self.queries.delete(item_id)
-        return self.render(status_code=200)
-        
-    
+
 class APISeveralHandler(AutoAPIBase):
     def get(self, item_ids):
         """ Return a list of resources
         """
         self.ids = item_ids.split(';')
-        instances = self.queries.getseveral(self.ids)
-        instances = json.dumps(instances)
+        try:
+            instances = self.queries.get_several(self.ids)
+        except KeyError:
+            #TODO: return representation of which items were found and which ones weren't
+            return self.render(status_code=404)
+
+        instances = json.dumps([instance.to_json() for instance in instances])
         self.add_to_payload('data', instances)
         return self.render(status_code=200)
+
     #def head(self):
         #return metadata about the resource - the headers should be the same as a get requets, but no content body
     def put(self, item_ids):
@@ -213,10 +267,10 @@ class APISeveralHandler(AutoAPIBase):
         valid, invalid = [], []
         data = json.loads(self.arguments['data'])
         for item in data:
-            #have we been given an id that was not requested
             shield = self.dicttype(**item)
             try:
-                id = shield[shield._meta['id_field']]
+                #have we been given an id that was not requested? Consume the ids on self to find out!
+                obj_id = shield.id
                 self.ids.remove(id)
             except ValueError:
                 #item included in data that shouldn't have been. Bail.
@@ -232,7 +286,11 @@ class APISeveralHandler(AutoAPIBase):
         if self.ids:
             return self.render(status_code=400)
         #commit this to the db
-        self.queries.save_several(valid)
+        try:
+            self.queries.save_several(valid)
+        except KeyError:
+            #TODO: return reprentation of which ones were successfully updated
+            return self.render(status_code=404)
 
         if not invalid:
             #we're all good, so return the all clear
@@ -248,9 +306,115 @@ class APISeveralHandler(AutoAPIBase):
             return self.render(status_code=400)
             
     def delete(self, item_ids):
-        self.ids = item_ids.split(';')
-        self.queries.delete_several(self.ids)
+        """ Deletes a list of items. this is really a cheat for a bunch of DELETE requests to foo/xxx
+        """
+        ids = item_ids.split(';')
+        try:
+            self.queries.delete_several(ids)
+        except KeyError:
+            return self.render(status_code=404)
         return self.render(status_code=200)
 
 
+class AbstractQueryset(object):
+    def __init__(self, db_conn=None):
+        self.db_conn = db_conn
 
+
+    ###
+    ### - Common API functions
+    ###
+
+    def save_single(self, shield, object_id=None):
+        """Takes a single dictshield and commits it to the db
+        Optional argument object_id  - if included, this is an update, and if missing this is a create
+        """
+        if object_id:
+            return self.update_single(shield, object_id)
+        else:
+            return self.create_single(shield)
+
+    ###
+    ### -  List API functions
+    ###
+
+    def create_single(self, shield):
+        raise NotImplementedError
+    
+    def get_list(self):
+        """Returns a list of objects in the db
+        """
+        raise NotImplementedError
+
+    ###
+    ### - Single API functions
+    ###
+
+
+    def update_single(self, shield, object_id):
+        raise NotImplementedError
+    
+    def get_single(self, item_id):
+        """ Pulls a single item out of the db and returns it
+        """
+        raise NotImplementedError
+
+
+    def delete_single(self, item_id):
+        """ Removes a single item from the db
+        """
+        raise NotImplementedError
+
+    
+    ###
+    ### - Several Item API functions
+    ###
+        
+    def save_several(self, shields):
+        """Takes a list of dictshield objects and serializes them to the db
+        """
+        raise NotImplementedError
+
+    def get_several(self, ids):
+        """Takes a list of ids, and returns a list of objects matching those ids
+        """
+        raise NotImplementedError
+
+    def delete_several(self, ids):
+        """Takes a list of ids and removes the items matching those ids from the database
+        """
+        raise NotImplementedError
+    
+
+
+def DictQueryset(AbstractQueryset):
+    def get_list(self):
+        return self.db_conn.iter_values()
+
+    def create_single(self, shield):
+        self.db_conn[id] = shield.to_python()
+        return self.db_conn[id]
+        
+    def update_single(self, shield, item_id):
+        self.db_conn[item_id] = shield.to_python()
+        return self.db_conn[item_id]
+
+    def get_single(self, item_id):
+        return self.db_conn[item_id]
+
+    def delete_single(self, item_id):
+        del self.db_conn[item_id]
+
+    def save_several(self, shields):
+        items = {}
+        for shield in shields:
+            items[shield.id] = shield.to_python()
+        self.db_conn.update(items)
+        return items
+
+    def get_several(self, ids):
+        return [self.db_conn[idz] for idz in ids]
+
+    def delete_several(self, ids):
+        for idz in ids:
+            del self.db_conn[id]
