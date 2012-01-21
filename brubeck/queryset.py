a2be8d4af94c4ce9f937272e1b49097c1b2263d4
James Dennis <jd@j2labs.net>
2012-01-20 20:34:05 -0500

    Cleaned up the code and added comments describing the ideas implemented.

--- brubeck/queryset.py
+++ brubeck/queryset.py
@@ -9,10 +9,46 @@ STATUS_FAILED = 'Failed'
 
     
 class AbstractQueryset(object):
+    """The design of the `AbstractQueryset` attempts to map RESTful calls
+    directly to CRUD calls. It also attempts to be compatible with a single
+    item of a list of items, handling multiple statuses gracefully if
+    necessary.
+
+    The querysets then must allow for calls to perform typical CRUD operations
+    on individual items or a list of items.
+
+    By nature of being dependent on complete data models or ids, the system
+    suggests users follow a key-value methodology. Brubeck believes this is
+    what we scale into over time and should just build to this model from the
+    start.
+
+    Implementing the details of particular databases is then to implement the
+    `create_one`, `create_many`, ..., for all the CRUD operations. MySQL,
+    Mongo, Redis, etc should be easy to implement while providing everything
+    necessary for a proper REST API.
+    """
     
     def __init__(self, db_conn=None, api_id='id'):
         self.db_conn = db_conn
         self.api_id = api_id
+
+    ###
+    ### CRUD Operations
+    ###
+
+    ### Section TODO:
+    ### * Pagination
+    ### * Hook in authentication
+    ### * Key filtering (owner / public)
+    ### * Make model instantiation an option
+
+    def create(self, shields):
+        """Commits a list of new shields to the database
+        """
+        if isinstance(shields, list):
+            return self.create_many(shields)
+        else:
+            return self.create_one(shields)
 
     def read(self, ids):
         """Returns a list of items that match ids
@@ -23,6 +59,34 @@ class AbstractQueryset(object):
             return self.read_many(ids)
         else:
             return self.read_one(ids)
+
+    def update(self, shields):
+        if isinstance(shields, list):
+            return self.update_many(shields)
+        else:
+            return self.update_one(shields)
+
+    def destroy(self, item_ids):
+        """ Removes items from the datastore
+        """
+        if isinstance(item_ids, list):
+            return self.destroy_many(item_ids)
+        else:
+            return self.destroy_one(item_ids)
+
+    ###
+    ### CRUD Implementations
+    ###
+
+    ### Create Functions
+    
+    def create_one(self, shield):
+        raise NotImplementedError
+
+    def create_many(self, shields):
+        raise NotImplementedError
+
+    ### Read Functions
 
     def read_all(self):
         """Returns a list of objects in the db
@@ -39,25 +103,7 @@ class AbstractQueryset(object):
         """
         raise NotImplementedError
 
-    def create(self, shields):
-        """Commits a list of new shields to the database
-        """
-        if isinstance(shields, list):
-            return self.create_many(shields)
-        else:
-            return self.create_one(shields)
-
-    def create_one(self, shield):
-        raise NotImplementedError
-
-    def create_many(self, shields):
-        raise NotImplementedError
-
-    def update(self, shields):
-        if isinstance(shields, list):
-            return self.update_many(shields)
-        else:
-            return self.update_one(shields)
+    ### Update Functions
 
     def update_one(self, shield):
         raise NotImplementedError
@@ -65,13 +111,7 @@ class AbstractQueryset(object):
     def update_many(self, shields):
         raise NotImplementedError
 
-    def destroy(self, item_ids):
-        """ Removes items from the datastore
-        """
-        if isinstance(item_ids, list):
-            return self.destroy_many(item_ids)
-        else:
-            return self.destroy_one(item_ids)
+    ### Destroy Functions
 
     def destroy_one(self, iid):
         raise NotImplementedError
@@ -81,11 +121,34 @@ class AbstractQueryset(object):
 
 
 class DictQueryset(AbstractQueryset):
+    """This class exists as an example of how one could implement a Queryset.
+    This model is an in-memory dictionary and uses the model's id as the key.
+
+    The data stored is the result of calling model's `to_python()` function.
+    """
     def __init__(self, **kw):
         """Set the db_conn to a dictionary.
         """
         super(DictQueryset, self).__init__(db_conn=dict(), **kw)
-    
+
+    ### Create Functions
+        
+    def create_one(self, shield):
+        if shield.id in self.db_conn:
+            status = STATUS_UPDATED
+        else:
+            status = STATUS_CREATED
+
+        shield_key = str(getattr(shield, self.api_id))
+        self.db_conn[shield_key] = shield.to_python()
+        return (status, shield)
+
+    def create_many(self, shields):
+        statuses = [self.create_one(shield) for shield in shields]
+        return statuses
+
+    ### Read Functions
+
     def read_all(self):
         return [(STATUS_OK, datum) for datum in self.db_conn.values()]
 
@@ -101,19 +164,7 @@ class DictQueryset(AbstractQueryset):
         except KeyError:
             raise FourOhFourException
 
-    def create_one(self, shield):
-        if shield.id in self.db_conn:
-            status = STATUS_UPDATED
-        else:
-            status = STATUS_CREATED
-
-        shield_key = str(getattr(shield, self.api_id))
-        self.db_conn[shield_key] = shield.to_python()
-        return (status, shield)
-
-    def create_many(self, shields):
-        statuses = [self.create_one(shield) for shield in shields]
-        return statuses
+    ### Update Functions
 
     def update_one(self, shield):
         shield_key = str(getattr(shield, self.api_id))
@@ -123,6 +174,8 @@ class DictQueryset(AbstractQueryset):
     def update_many(self, shields):
         statuses = [self.update_one(shield) for shield in shields]
         return statuses
+
+    ### Destroy Functions
 
     def destroy_one(self, item_id):
         try:
