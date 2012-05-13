63b08653a709448239cd13e94e5d97be76fd806d
Isaac Gonzalez <hyperneato@gmail.com>
2012-05-13 16:59:52 -0400

    first pass at RedisQueryset

--- brubeck/queryset.py
+++ brubeck/queryset.py
@@ -1,5 +1,6 @@
 from request_handling import FourOhFourException
-
+from itertools import imap
+import zlib
 
 class AbstractQueryset(object):
     """The design of the `AbstractQueryset` attempts to map RESTful calls
@@ -26,6 +27,7 @@ class AbstractQueryset(object):
     MSG_CREATED = 'Created'
     MSG_NOTFOUND = 'Not Found'
     MSG_FAILED = 'Failed'
+    MSG_EXISTS = 'Exists'
 
     def __init__(self, db_conn=None, api_id='id'):
         self.db_conn = db_conn
@@ -185,3 +187,110 @@ class DictQueryset(AbstractQueryset):
     def destroy_many(self, ids):
         statuses = [self.destroy_one(iid) for iid in ids]
         return statuses
+
+class RedisQueryset(AbstractQueryset):
+    """This class uses redis to store the DictShield
+    This model is an in-memory dictionary and uses the model's id as the key.
+
+    The data stored is the result of calling model's `to_python()` function.
+
+    Redis connection uses the redis-py api located here:
+    https://github.com/andymccurdy/redis-py
+
+    All shield ids are assumed to be strings
+    """
+    # TODO: - catch connection exceptions?
+    #       - set Redis EXPIRE and self.expires
+    #       - are shield ids always strings or numbers or either?
+    #         - should getattr(shield, self.api_id) be used as the Redis hash key?
+    #      
+    def __init__(self, compress=False, compress_level=1):
+        """The Redis connection wiil be passed in **kw and is used below
+        as self.db_conn.
+        """
+        super(AbstractQueryset, self).__init__(**kw)
+        self.compress = compress
+        self.compress_level = compress_level
+
+    def _setvalue(self, shield):
+        if self.compress:
+            return zlib.compress(shield.to_json(), self.compress_level)
+        return shield.to_json()
+
+    def _readvalue(self, value):
+        if self.compress:
+            return zlib.decompress(value)
+        return value
+
+    def _message_factory(self, fail_status, success_status):
+        """A Redis value returns 1 or 0 upon success or failure, respectively
+        """
+        return lambda x: success_status if x else fail_status
+
+    ### Create Functions
+
+    def create_one(self, shield):
+        shield_value = self._value(shield)
+        
+        if self.db_conn.hexists(self.api_id, shield.id):
+            return (self.MSG_EXISTS, shield)
+
+        self.db_conn.hset(self.api_id, shield.id, shield_value)
+        return (self.MSG_CREATED, shield)
+
+    def create_many(self, shields):
+        message_handler = self._message_factory(self.MSG_EXISTS, self.MSG_CREATED)
+        pipe = self.db_conn.pipeline()
+        [pipe.hsetnx(self.api_id, shield.id, self._value(shield)) for shield in shields]
+        results = zip(imap(message_handler, pipe.execute()), shields)
+        pipe.reset()
+        return results
+
+    ### Read Functions
+
+    def read_all(self):
+        return [(self.MSG_OK, datum) for datum in self.db_conn.hgetall(self.api_id)]
+
+    def read_one(self, shield_id):
+        if self.db_conn.hexists(self.api_id, shield_id):
+            return (self.MSG_OK, _readvalue(self.db_conn.hget(self.api_id, shield_id)))
+        else:
+            return (self.MSG_NOTFOUND, shield_id)
+
+    def read_many(self, shield_ids):
+        message_handler = self._message_factory(self.MSG_NOTFOUND, self.MSG_OK)
+        pipe = self.db_conn.pipeline()
+        [pipe.hget(self.api_id, shield_id) for shield_id in shield_ids]
+        results = pipe.execute()
+        pipe.reset()
+        return zip(imap(message_handler, results), map(_readvalue, results))
+
+
+    ### Update Functions
+    def update_one(self, shield):
+        message_handler = _message_factory(self.MSG_NOTFOUND, self.MSG_UPDATED)
+        return (message_handler(self.db_conn.hsetnx(self.api_id, shield.id)), shield)
+
+    def update_many(self, shields):
+        message_handler = _message_factory(self.MSG_NOTFOUND, self.MSG_UPDATED)
+        pipe = self.db_conn.pipeline()
+        [pipe.hsetnx(self.api_id, shield.id, _value(shield)) for shield in shields]
+        results = pipe.execute()
+        pipe.reset()
+        return imap(message_handler, results)
+
+    ### Destroy Functions
+
+    def destroy_one(self, shield_id):
+        if self.db_conn.hdel(self.api_id, shield_id):
+            return self.MSG_OK
+        return self.MSG_NOTFOUND
+
+    def destroy_many(self, ids):
+        message_handler = _message_factory(self.MSG_NOTFOUND, self.MSG_OK)
+        pipe = self.db_conn.pipeline()
+        [pipe.hdel(self.api_id, _id) for _id in ids]
+        results = pipe.execute()
+        pipe.reset()
+        return imap(message_handler, results)
+
