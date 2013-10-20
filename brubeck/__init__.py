6cb5760dc0b270b24da233072bb05850439663dd
James Dennis <jd@j2labs.net>
2013-10-20 18:21:09 -0400

    Preparing for the new, streamlined Brubeck that will be 0.5

--- brubeck/__init__.py
+++ brubeck/__init__.py
@@ -2,15 +2,9 @@
 
 """The Brubeck Message Handling system."""
 
-version = "0.4.0"
-version_info = (0, 4, 0)
-__all__ = ['auth',
-           'autoapi',
-           'caching',
-           'datamosh',
-           'models',
-           'mongrel2',
-           'queryset',
+version = "0.5.0"
+version_info = (0, 5, 0)
+__all__ = ['connections',
+           'request',
            'request_handling',
-           'templating',
            'timekeeping']
