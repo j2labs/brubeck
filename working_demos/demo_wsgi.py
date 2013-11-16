d5538d70bb3135a5513d9f3bd41439951041e4c4
Jms Dnns <jdennis@gmail.com>
2013-11-16 16:26:28 -0500

    Added a demos directory to illustrate what it looks like to work with the new design of Brubeck. This directory is temporary and will likely be removed when documentation catches up

---
+++ working_demos/demo_wsgi.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from brubeck.servers import Brubeck
+from brubeck.handlers import WebMessageHandler
+from brubeck.connections import WSGIConnection
+
+
+class DemoHandler(WebMessageHandler):
+    def get(self):
+        name = self.get_argument('name', 'dude')
+        self.set_body('Take five, %s!' % name)
+        return self.render()
+
+    
+config = {
+    'msg_conn': WSGIConnection(),
+    'handler_tuples': [(r'^/brubeck', DemoHandler)],
+}
+
+
+app = Brubeck(**config)
+app.run()
